c9833efe223860721b57917ae3396a898a86411e
PedroTH07 <pedro.mape7@gmail.com>
2026-01-30 16:32:03 -0300

    test: refactoring emailDto to class attribute

--- src/test/java/com/mycroservice/emailsender/services/EmailServiceTest.java
+++ src/test/java/com/mycroservice/emailsender/services/EmailServiceTest.java
@@ -8,6 +8,7 @@ import jakarta.mail.Session;
 import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -42,16 +43,15 @@ class EmailServiceTest {
 
     @Nested
     class send {
+        private final EmailRequestDto emailDto = new EmailRequestDto(
+                List.of("pedro.mape7@gmail.com"),
+                "subject",
+                "body string");
 
         @Test
         @DisplayName("Should send an email with success with html body")
         void shouldSendEmailSuccessHtmlBody() throws Exception {
             // Arrange
-            var emailDto = new EmailRequestDto(
-                    List.of("pedro.mape7@gmail.com"),
-                    "subject",
-                    "body"
-            );
             var emailContent = "<!DOCTYPE html><html><head></head><body>Oi</body></html>";
             MultipartFile html = new MockMultipartFile(
                     "html",
@@ -87,11 +87,6 @@ class EmailServiceTest {
         @DisplayName("Should send an email with success with String body")
         void shouldSendEmailSuccessStringBody() throws Exception {
             // Arrange
-            var emailDto = new EmailRequestDto(
-                    List.of("pedro.mape7@gmail.com"),
-                    "subject",
-                    "body string"
-            );
             var session = Session.getInstance(new Properties());
             when(sender.createMimeMessage()).thenReturn(new MimeMessage(session));
             doNothing().when(sender).send(mimeMessageCaptor.capture());
@@ -120,11 +115,6 @@ class EmailServiceTest {
         @DisplayName("Should return null when throws messaging exception")
         void shouldReturnNullOnMessagingException() throws Exception {
             // Arrange
-            var emailDto = new EmailRequestDto(
-                    List.of("pedro.mape7@gmail.com"),
-                    "subject",
-                    "body"
-            );
             doThrow(MessagingException.class).when(service).createHelper(any());
 
             // Act
@@ -140,11 +130,6 @@ class EmailServiceTest {
         @DisplayName("Should throw exception when error occurs")
         void shouldThrowsExceptionWhenErrorOccurs() throws Exception {
             // Arrange
-            var emailDto = new EmailRequestDto(
-                    List.of("pedro.mape7@gmail.com"),
-                    "subject",
-                    "body"
-            );
             var session = Session.getInstance(new Properties());
             when(sender.createMimeMessage()).thenReturn(new MimeMessage(session));
             doThrow(RuntimeException.class).when(sender).send(any());
